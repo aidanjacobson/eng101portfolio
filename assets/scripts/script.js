8e2c373c37bf6f9924399da650b47f44b237c13b
Aidan <survivalking4@gmail.com>
2021-01-19 01:55:02 -0700

    added page switching based on location.hash

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
 function doUpdateFocus(e) {
+    /*
     var cardElements = document.getElementsByClassName("card");
     var cardRefElements = document.getElementsByClassName("cardref");
     for (var i = 0; i < cardElements.length; i++) {
@@ -9,4 +10,34 @@ function doUpdateFocus(e) {
     }
     e.target.classList.add("selected");
     document.querySelector(e.target.getAttribute("data-href")).classList.add("selected");
+    */
+   //elementClick(e.target);
+   location.hash = e.target.getAttribute("data-href");
+   window.onhashchange();
+}
+
+function elementClick(elementHash) {
+    var cardElements = document.getElementsByClassName("card");
+    var cardRefElements = document.getElementsByClassName("cardref");
+    console.log(elementHash);
+    for (var i = 0; i < cardElements.length; i++) {
+        cardElements[i].classList.remove("selected");
+        if ("#" + cardElements[i].id == elementHash) {
+            cardElements[i].classList.add("selected");
+        }
+    }
+    for (var i = 0; i < cardRefElements.length; i++) {
+        cardRefElements[i].classList.remove("selected");
+        if (cardRefElements[i].getAttribute("data-href") == elementHash) {
+            cardRefElements[i].classList.add("selected");
+        }
+    }
+}
+window.onhashchange = function(e) {
+    elementClick(location.hash);
+}
+window.onload = function() {
+    if (location.hash == "") {
+        location.hash = "#aboutme";
+    }
 }
